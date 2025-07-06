c0edd22b4ca0a89f6076a57d3b23c9ef2ea8894b
usman-temp <usmanjaved1620@gmail.com>
2025-07-06 20:18:23 +0500

    Create src/app/usman/page.tsx

---
+++ src/app/usman/page.tsx
@@ -0,0 +1,9 @@
+export default function UsmanPage() {
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <h1 className="text-6xl font-bold bg-gradient-to-r from-red-500 to-green-500 text-transparent bg-clip-text">
+        Usman
+      </h1>
+    </div>
+  );
+}
